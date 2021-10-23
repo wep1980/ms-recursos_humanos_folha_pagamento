4972d7e7671d20d42376de4196686d22433d6fa9
wep1980 <wepbike@gmail.com>
2021-10-23 06:47:51 -0300

    Configurando como cliente Eureka

--- src/main/java/br/com/wepdev/recursoshumanosfolhapagamento/RecursosHumanosFolhaPagamentoApplication.java
+++ src/main/java/br/com/wepdev/recursoshumanosfolhapagamento/RecursosHumanosFolhaPagamentoApplication.java
@@ -2,12 +2,14 @@ package br.com.wepdev.recursoshumanosfolhapagamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-// Esse projeto vai ser um cliente Ribbon, cliente do micro servico de trabalhadores, que tera varias instancias
-@RibbonClient(name = "recursos-humanos-trabalhadores")
+// Esse projeto vai ser um cliente Ribbon, cliente do micro servico de trabalhadores, que tera varias instancias, e faz o balanceamento de carga
+//@RibbonClient(name = "recursos-humanos-trabalhadores")
 @EnableFeignClients // Habilitando o projeto para utilização do Feign
+@EnableEurekaClient // Habilitando o projeto como cliente Eureka
 @SpringBootApplication
 public class RecursosHumanosFolhaPagamentoApplication {
 
