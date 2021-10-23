9a5e5070862d3cb5f80cba6213ca5a96b1a71286
wep1980 <wepbike@gmail.com>
2021-10-23 07:39:25 -0300

    Configurando Hystrix e seu timeout, criando endpoint alternativo para caso ocorra falhas

--- src/main/java/br/com/wepdev/recursoshumanosfolhapagamento/RecursosHumanosFolhaPagamentoApplication.java
+++ src/main/java/br/com/wepdev/recursoshumanosfolhapagamento/RecursosHumanosFolhaPagamentoApplication.java
@@ -2,6 +2,7 @@ package br.com.wepdev.recursoshumanosfolhapagamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 //@RibbonClient(name = "recursos-humanos-trabalhadores")
 @EnableFeignClients // Habilitando o projeto para utilização do Feign
 @EnableEurekaClient // Habilitando o projeto como cliente Eureka
+@EnableCircuitBreaker
 @SpringBootApplication
 public class RecursosHumanosFolhaPagamentoApplication {
 
--- src/main/java/br/com/wepdev/recursoshumanosfolhapagamento/resources/PagamentoResource.java
+++ src/main/java/br/com/wepdev/recursoshumanosfolhapagamento/resources/PagamentoResource.java
@@ -2,6 +2,7 @@ package br.com.wepdev.recursoshumanosfolhapagamento.resources;
 
 import br.com.wepdev.recursoshumanosfolhapagamento.entities.Pagamento;
 import br.com.wepdev.recursoshumanosfolhapagamento.services.PagamentoService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,12 +19,50 @@ public class PagamentoResource {
     private PagamentoService pagamentoService;
 
 
-
+    /**
+     * Esse endpoint chama o endpoint do micro serviço de trabalhador
+     */
+    @HystrixCommand(fallbackMethod = "getPagamentoAlternativo") // configura o nome do metodo alternativo caso ocorra alguma falha na chamada desse end point
     @GetMapping(value = "/{trabalhadorId}/dias/{dias}")
     public ResponseEntity<Pagamento> getPagamento(@PathVariable Long trabalhadorId, @PathVariable Integer dias){
         Pagamento pagamento = pagamentoService.getPagamento(trabalhadorId, dias);
 
         return ResponseEntity.ok(pagamento);
-
     }
+
+
+    /**
+     * Endpoint alternativo caso falhe o endpoint principal -> getPagamento()
+     * @param trabalhadorId
+     * @param dias
+     * @return
+     */
+    public ResponseEntity<Pagamento> getPagamentoAlternativo(Long trabalhadorId, Integer dias){
+        Pagamento pagamento = new Pagamento("Nome criado pelo metodo alternativo do micro serviço folha de pagamento", 400.0, dias);
+        return ResponseEntity.ok(pagamento);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
